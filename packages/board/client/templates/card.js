f4a88cfe49df56350b6db0de51ac137d636d404a
Vitor HP Bittencourt <vitorhp2@gmail.com>
2016-03-06 11:32:47 -0300

    Stopped rotating every card based on type_code. Only applying this on the Corp's remoteServers' ice

--- packages/board/client/templates/card.js
+++ packages/board/client/templates/card.js
@@ -3,9 +3,5 @@ Template.card.helpers({
   cardImg(card, rezzed=true) {
     if (card)
       return rezzed ? card.imgSrc : card.backImgSrc()
-  },
-
-  classes(card) {
-    return card && (card.type_code === "ice") ? "card--ice" : ""
   }
 })
