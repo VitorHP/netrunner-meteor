89f2e2394f36bd9eb6b6d958136e9ccea7df303f
Vitor HP Bittencourt <vitorhp2@gmail.com>
2016-03-09 22:55:00 -0300

    Refactored seed data

--- packages/board/server/seeds.js
+++ packages/board/server/seeds.js
@@ -1,39 +1,41 @@
-// Game 
-Game.remove("7MGuiovynhY2TgsbJ")
+function seedData(collection, data) {
+  let ids = data.map(function(d){ return d["_id"] })
 
-Game.insert({
+  ids.forEach(function(id){ collection.remove(id) })
+
+  data.forEach(function(item){
+    collection.insert(item)
+  })
+}
+
+// Game 
+
+seedData(Game, [{
   "_id" : "7MGuiovynhY2TgsbJ",
   "runnerId" : "7MGuiovynhY2TgsbJ",
   "corpId" : "7MGuiovynhY2TgsbJ",
   "turnOwner" : "corp"
-})
+}])
 
 // Decks
 
-Decks.remove({ _id: "sfHfp6WobMieubfYZ"})
-Decks.remove({ _id: "56d1aef3d330b6eba537beaa"})
-
-Decks.insert({
-  "_id" : "sfHfp6WobMieubfYZ",
-  "name" : "Haas Bioroid",
-  "identityCardCode" : 54,
-  "cardCodes" : [54, 55, 63],
-  "side_code" : "corp"
-})
-
-Decks.insert({
-  "_id" : "56d1aef3d330b6eba537beaa",
-  "name" : "Noise",
-  "identityCardCode" : 1,
-  "cardCodes" : [1, 7, 23, 31],
-  "side_code" : "runner"
-})
+seedData(Decks, [{
+    "_id" : "sfHfp6WobMieubfYZ",
+    "name" : "Haas Bioroid",
+    "identityCardCode" : 54,
+    "cardCodes" : [54, 55, 63],
+    "side_code" : "corp"
+  }, {
+    "_id" : "56d1aef3d330b6eba537beaa",
+    "name" : "Noise",
+    "identityCardCode" : 1,
+    "cardCodes" : [1, 7, 23, 31],
+    "side_code" : "runner"
+}])
 
 // Runner
 
-Runner.remove("7MGuiovynhY2TgsbJ")
-
-Runner.insert({
+seedData(Runner, [{
   "_id" : "7MGuiovynhY2TgsbJ",
   "deckId" : "56d1aef3d330b6eba537beaa",
   "backgroundImgSrc" : "images/cards/runner-background.png",
@@ -46,12 +48,11 @@ Runner.insert({
   "programs" : [],
   "hardware" : [],
   "resources" : []
-})
+}])
 
 // Corp
 
-Corp.remove("7MGuiovynhY2TgsbJ")
-Corp.insert({
+seedData(Corp, [{
   "_id" : "7MGuiovynhY2TgsbJ",
   "deckId" : "sfHfp6WobMieubfYZ",
   "backgroundImgSrc" : "images/cards/corp-background.png",
@@ -62,19 +63,11 @@ Corp.insert({
   "hand" : [55, 63],
   "identityCardCode" : 54,
   "remoteServers" : []
-})
+}])
 
 // Cards
 
-Cards.remove("3gRNkaG99J5goYjKD")
-Cards.remove("tzJTsy5LxBj7FNhhc")
-Cards.remove("8dXPsvfXFmCeHHnHf")
-Cards.remove("56d1aef3d330b6eba537beaa")
-Cards.remove("56dafef188583c47259f9dd1")
-Cards.remove("56db38b58820a734731a09ab")
-Cards.remove("56db38c78820a734731a09ac")
-
-Cards.insert({
+seedData(Cards, [{
   "_id" : "3gRNkaG99J5goYjKD",
   "title" : "Noise - Hacker Extraordinaire",
   "side" : "Runner",
@@ -84,9 +77,7 @@ Cards.insert({
   "imgSrc" : "images/cards/core/bc0f047c-01b1-427f-a439-d451eda01001.jpg",
   "code" : 1,
   "type_code" : "identity"
-})
-
-Cards.insert({
+}, {
   "_id" : "tzJTsy5LxBj7FNhhc",
   "title" : "Corroder",
   "side" : "Runner",
@@ -96,9 +87,7 @@ Cards.insert({
   "imgSrc" : "images/cards/core/bc0f047c-01b1-427f-a439-d451eda01007.jpg",
   "code" : 7,
   "type_code" : "program"
-})
-
-Cards.insert({
+}, {
   "_id": "56db38b58820a734731a09ab",
   "title": "Lemuria Codecracker",
   "type": "Hardware",
@@ -107,9 +96,7 @@ Cards.insert({
   "faction_code": "criminal",
   "imgSrc" : "images/cards/core/bc0f047c-01b1-427f-a439-d451eda01023.jpg",
   "code": 23
-})
-
-Cards.insert({
+}, {
   "_id": "56db38c78820a734731a09ac",
   "title": "Data Dealer",
   "type": "Resource",
@@ -118,9 +105,7 @@ Cards.insert({
   "faction_code": "criminal",
   "imgSrc" : "images/cards/core/bc0f047c-01b1-427f-a439-d451eda01031.jpg",
   "code": 31
-})
-
-Cards.insert({
+}, {
   "_id" : "8dXPsvfXFmCeHHnHf",
   "title" : "Haas-Bioroid - Engineering the future",
   "side" : "Corp",
@@ -130,9 +115,7 @@ Cards.insert({
   "imgSrc" : "images/cards/core/bc0f047c-01b1-427f-a439-d451eda01054.jpg",
   "code" : 54,
   "type_code" : "identity"
-})
-
-Cards.insert({
+}, {
   "_id" : "56d1aef3d330b6eba537beaa",
   "title" : "Accelerated Beta Test",
   "side" : "Corp",
@@ -142,9 +125,7 @@ Cards.insert({
   "imgSrc" : "images/cards/core/bc0f047c-01b1-427f-a439-d451eda01055.jpg",
   "code" : 55,
   "type_code" : "agenda"
-})
-
-Cards.insert({
+}, {
   "_id" : "56dafef188583c47259f9dd1",
   "title": "Viktor 1.0",
   "side" : "Corp",
@@ -154,4 +135,4 @@ Cards.insert({
   "imgSrc" : "/images/cards/core/bc0f047c-01b1-427f-a439-d451eda01063.jpg",
   "code": 63,
   "type_code": "ice",
-})
+}])
