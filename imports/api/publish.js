5fd92f192e25662b2949228727dccf6b9609cf01
Vitor HP Bittencourt <vitorhp2@gmail.com>
2016-04-08 10:21:10 -0300

    Refactored imports/api/publish.js

--- imports/api/publish.js
+++ imports/api/publish.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import 'meteor/reywood:publish-composite';
 
 import { Runner } from './runners/runners.js';
@@ -6,43 +7,32 @@ import { Game } from './games/games.js';
 import { Decks } from './decks/decks.js';
 import { Cards } from './cards/cards.js';
 
-function deckCardCodesList (deck_cards) {
-  return Object.keys(deck_cards).reduce(function(card_codes, card_code){
-    card_codes = card_codes.concat(new Array(deck_cards[card_code]).fill(card_code))
-
-    return card_codes
-  }, [])
-}
-
-Meteor.publishComposite('Game.game', function(deckName){
-
-  return {
-    find () {
-      return Game.find({ _id: "7MGuiovynhY2TgsbJ" })
+Meteor.publishComposite('Game.game', () => ({
+  find() {
+    return Game.find({ _id: '7MGuiovynhY2TgsbJ' });
+  },
+  children: [{
+    find(game) {
+      return Corp.find({ _id: game.corp_id });
     },
     children: [{
-      find (game) {
-        return Corp.find({ _id: game.corp_id })
-      },
-      children: [{
-        find(corp) {
-          let card_codes = Object.keys(Decks.findOne(corp.deck_id).cards)
+      find(corp) {
+        const cardCodes = Object.keys(Decks.findOne(corp.deck_id).cards);
 
-          return Cards.find({ code: { "$in": card_codes } })
-        }
-      }]
-    },{
-      find (game) {
-        return Runner.find({ _id: game.runner_id })
+        return Cards.find({ code: { $in: cardCodes } });
       },
-      children: [{
-        find(runner) {
-          let card_codes = Object.keys(Decks.findOne(runner.deck_id).cards)
+    }],
+  }, {
+    find(game) {
+      return Runner.find({ _id: game.runner_id });
+    },
+    children: [{
+      find(runner) {
+        const cardCodes = Object.keys(Decks.findOne(runner.deck_id).cards);
 
-          return Cards.find({ code: { "$in": card_codes } })
-        }
-      }]
-    }]
-  }
-})
+        return Cards.find({ code: { $in: cardCodes } });
+      },
+    }],
+  }],
+}));
 
