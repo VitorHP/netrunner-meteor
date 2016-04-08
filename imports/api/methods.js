332984c6a58629189101fb53b7a459a85c47cb14
Vitor HP Bittencourt <vitorhp2@gmail.com>
2016-04-08 01:47:48 -0300

    imports/api/methods.js refactoring

--- imports/api/methods.js
+++ imports/api/methods.js
@@ -1,36 +1,30 @@
-import { Game } from "./games/games.js";
-import { Runner } from "./runners/runners.js";
-import { Corp } from "./corps/corps.js";
+import { Meteor } from 'meteor/meteor';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
+import { Game } from './games/games.js';
+import { Runner } from './runners/runners.js';
+import { Corp } from './corps/corps.js';
 
 Meteor.methods({
   'Runner.methods.update'({ runner_id, newRunner }) {
-
-    new SimpleSchema({ runner_id: { type: String } }).validate({runner_id});
-    Runner.schema.validate(newRunner)
-
-    const runner = Runner.findOne(runner_id);
+    new SimpleSchema({ runner_id: { type: String } }).validate({ runner_id });
+    Runner.schema.validate(newRunner);
 
     Runner.update(runner_id, newRunner);
   },
 
   'Corp.methods.update'({ corp_id, newCorp }) {
-
-    new SimpleSchema({ corp_id: { type: String } }).validate({corp_id});
-    Corp.schema.validate(newCorp)
-
-    const corp = Corp.findOne(corp_id);
+    new SimpleSchema({ corp_id: { type: String } }).validate({ corp_id });
+    Corp.schema.validate(newCorp);
 
     Corp.update(corp_id, newCorp);
   },
 
   'Game.methods.update'({ gameId, newGame }) {
-
-    new SimpleSchema({ gameId: { type: String } }).validate({gameId});
-    Game.schema.validate(newGame)
-
-    const game = Game.findOne(gameId);
+    new SimpleSchema({ gameId: { type: String } }).validate({ gameId });
+    Game.schema.validate(newGame);
 
     Game.update(gameId, newGame);
-  }
+  },
 });
 
